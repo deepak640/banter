069c04b6e6d5a70ccc041bb12b36543d982b5f1a
deepaknegi701 <deepak.negi@ebslon.com>
2025-07-06 10:59:45 +0530

    refactor: remove dist directory and update .gitignore to exclude it

--- backend/dist/helpers/unqiueCode.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateUniqueCode = void 0;
-const generateUniqueCode = () => {
-    const code = `${Date.now()}${Math.floor(Math.random() * 1000)}`;
-    return code.substring(code.length - 4);
-};
-exports.generateUniqueCode = generateUniqueCode;

12e93ea137956c73e84ce1bd0708aa34ff1a363d
deepaknegi701 <deepak.negi@ebslon.com>
2025-07-08 00:32:18 +0530

    refactor: remove compiled JavaScript files from dist directory

--- backend/dist/v1/routes/message.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const message_controller_1 = require("../controllers/message.controller");
-const router = (0, express_1.Router)();
-router.get("/:conversationId", message_controller_1.getAllMessages);
-exports.default = router;

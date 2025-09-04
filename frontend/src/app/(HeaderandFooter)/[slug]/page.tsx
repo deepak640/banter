cd0b4132cb93d6b659655ffae564c182ab34f88c
deepaknegi701 <deepak.negi@ebslon.com>
2025-09-04 22:23:27 +0530

    fixed slug issue

--- frontend/src/app/(HeaderandFooter)/[slug]/page.tsx
+++ frontend/src/app/(HeaderandFooter)/[slug]/page.tsx
@@ -1,7 +1,8 @@
 import Chat from '@/app/_components/Chat'
 import React from 'react'
 
-export default function Page({ params }: any) {
+export default async function Page({ params: paramsPromise }: { params: Promise<{ slug: string }> }) {
+  const params = await paramsPromise;
   return (
     <Chat slug={params.slug} />
   )
